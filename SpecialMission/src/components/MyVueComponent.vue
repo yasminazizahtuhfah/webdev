32dada34e2e71017d4827116cb63f4e7ab7a8136
yasminazizahtuhfah <95863438+yasminazizahtuhfah@users.noreply.github.com>
2023-09-12 20:40:16 +0700

    Add files via upload

---
+++ SpecialMission/src/components/MyVueComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Data dari MongoDB</h1>
+      <ul>
+        <li v-for="item in data" :key="item._id">{{ item.nama }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        data: [],
+      };
+    },
+    mounted() {
+      // Ambil data dari server backend
+      axios.get('/api/data').then((response) => {
+        this.data = response.data;
+      });
+    },
+  };
+  </script>
+  
