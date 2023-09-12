601038d2ea0dc544d5ab6e12a0d0d1cf5d29ed15
yasminazizahtuhfah <95863438+yasminazizahtuhfah@users.noreply.github.com>
2023-09-12 20:39:49 +0700

    Add files via upload

---
+++ SpecialMission/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+      <!-- Gunakan komponen di sini -->
+      <MyVueComponent />
+    </div>
+  </template>
+  
+  <script>
+  import MyVueComponent from '@/components/MyVueComponent.vue';
+  
+  export default {
+    components: {
+      MyVueComponent,
+    },
+  };
+  </script>
+  
