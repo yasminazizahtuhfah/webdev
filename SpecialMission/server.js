992dbc39aba0c5e3badb632959a42ffe9be35fa4
yasminazizahtuhfah <95863438+yasminazizahtuhfah@users.noreply.github.com>
2023-09-12 20:34:46 +0700

    Add files via upload

--- SpecialMission/server.js
+++ SpecialMission/server.js
@@ -1,1 +1,28 @@
+var express = require('express');
+var server = express();
+var routes = require('./routes/routes');
+var mongoose = require('mongoose');
+const cors = require('cors');
 
+mongoose.connect("mongodb://127.0.0.1:27017/ysm", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("DB connected");
+})
+.catch((error) => {
+  console.error("DB connection error:", error);
+});
+
+server.use(express.json());
+server.use(routes);
+server.use(cors())
+
+server.listen(8000, function (error) {
+  if (error) {
+    console.log("error");
+  } else {
+    console.log("start");
+  }
+});
